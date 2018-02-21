40f1f20b940737721ac34ac90092b22af842a1be
WhereIsWerebear <WhereIsWerebear@protonmail.com>
2018-02-21 13:58:32 -0500

    Add fmtHex() and detect ranges of readable memory

--- Win32 API test/Win32 API test.h
+++ Win32 API test/Win32 API test.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <string>
+
+std::string fmtHex(uint64_t n);
+
 SIZE_T tryReadProcessMemory(HANDLE hProcess, LPCVOID baseAddress, LPVOID buffer, SIZE_T bufferSize, bool reportError = false);
 
 void reportWinError(DWORD error);
